744d13c5d6b91a3e891c2622c8d83651b7dd9a84
Chance Vodnoy <esgameco@gmail.com>
2021-07-21 20:14:53 -0700

    Added cookie parsing capability

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,15 @@ import path from 'path';
 const app: Application = express();
 const port: number = 8080;
 
+// Define paths
+const publicDir = path.join(__dirname, 'public');
+
 // Usages
 app.use(cookieParser()); // Cookie Parser
-app.use(express.static(path.join(__dirname, 'public'))) // Public directory
+app.use(express.static(publicDir)) // Public directory
 
 app.get('/', (req: Request, res: Response) => {
-    res.sendFile('./public/index.html');
+    res.sendFile(path.join(publicDir, 'html/index.html'));
 });
 
 app.listen(port, () => console.log('Connected on', port));
