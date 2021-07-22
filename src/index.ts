680fc5af0118eee6bcbce5dd6d195a54e063979e
Chance Vodnoy <esgameco@gmail.com>
2021-07-21 20:01:36 -0700

    Added base html and css files

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,19 @@ import express, {
     Request,
     Response
 } from 'express';
+import cookieParser from 'cookie-parser';
+import path from 'path';
 
+// Define app
 const app: Application = express();
 const port: number = 8080;
 
+// Usages
+app.use(cookieParser()); // Cookie Parser
+app.use(express.static(path.join(__dirname, 'public'))) // Public directory
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('test')
+    res.sendFile('./public/index.html');
 });
 
 app.listen(port, () => console.log('Connected on', port));
